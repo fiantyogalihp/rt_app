89be596220aaf494b000020e9283fd1cd4b4030f
fiantyogalihp <fiantiogalih@gmail.com>
2022-06-15 12:51:39 +0700

    add configuration

--- lib/configuration/configuration.dart
+++ lib/configuration/configuration.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget addVerticalSpace(double height) {
@@ -10,4 +11,22 @@ Widget addHorizontalSpace(double width) {
   return SizedBox(
     width: width,
   );
+}
+
+Future<dynamic> errorMessage(
+    String title, String desc, BuildContext context) async {
+  return await showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Empty $title'),
+          content: Text(desc),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      });
 }
